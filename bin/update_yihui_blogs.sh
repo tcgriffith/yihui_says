895fb42a6b4b78fb9de322cd2635daffe684bdee
tcgriffith <ztc.ustcer@gmail.com>
2019-12-06 15:45:25 +1000

    stageit

--- bin/update_yihui_blogs.sh
+++ bin/update_yihui_blogs.sh
@@ -1,28 +1,40 @@
+## Relative path used, run this script in bin/
 
-blogcontent=~/GIT/yihui_says/data/allposts.txt
-blogcontentcln=~/GIT/yihui_says/data/allposts.min.txt
+blogcontent=../data/allposts.txt
+blogcontentcln=../data/allposts.min.txt
+tmpjs=../js/quotes2.js # our quote generator
+# tmpjs2=../js/quotes.js 
 
-tmpjs=~/GIT/yihui_says/js/quotes2.js
-tmpjs2=~/GIT/yihui_says/js/quotes.js
+
+
+## get yihui's posts
 
 mkdir -p /tmp/_tmpdir
-
-
 git clone https://github.com/rbind/yihui /tmp/_tmpdir
-
 cat /tmp/_tmpdir/content/cn/2*.md > $blogcontent
 
 
-echo " var mydata = [" > $tmpjs
+
+# extract quotes
 
 cat $blogcontent |grep -v "^[title|date|slug|\\\-\\\-\\\-]" |grep "^>"  |sed "s/\"//g" > $blogcontentcln
 
 
+# generate the quote generator
+
+echo " var mydata = [" > $tmpjs
 cat $blogcontentcln |awk '{if (length($0) >10) printf "\"%s\",\n",$0}' >> $tmpjs
-
 echo "]" >> $tmpjs
-
 cat $tmpjs2 >> $tmpjs
+
+echo "
+ function Start () {
+     var myrandom = Math.floor( Math.random() * (mydata.length));
+     document.getElementById('quotedisplay').innerHTML = mydata[myrandom]
+ }
+
+ Start();" >> $tmpjs
+
 
 rm -rf /tmp/_tmpdir
 
