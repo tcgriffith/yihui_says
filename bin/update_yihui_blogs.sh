152c2d43aaf275934b32f60b79f2f092e8ec61c0
tcgriffith <ztc.ustcer@gmail.com>
2019-12-06 15:22:26 +1000

    changes

--- bin/update_yihui_blogs.sh
+++ bin/update_yihui_blogs.sh
@@ -2,6 +2,9 @@
 blogcontent=~/GIT/yihui_says/data/allposts.txt
 blogcontentcln=~/GIT/yihui_says/data/allposts.min.txt
 
+tmpjs=~/GIT/yihui_says/js/quotes2.js
+tmpjs2=~/GIT/yihui_says/js/quotes.js
+
 mkdir -p /tmp/_tmpdir
 
 
@@ -9,8 +12,17 @@ git clone https://github.com/rbind/yihui /tmp/_tmpdir
 
 cat /tmp/_tmpdir/content/cn/2*.md > $blogcontent
 
-cat $blogcontent |grep -v "^[title|date|slug|\\\-\\\-\\\-]" |grep "^>" |awk '{if (length($0) >10) print}' > $blogcontentcln
 
+echo " var mydata = [" > $tmpjs
+
+cat $blogcontent |grep -v "^[title|date|slug|\\\-\\\-\\\-]" |grep "^>"  |sed "s/\"//g" > $blogcontentcln
+
+
+cat $blogcontentcln |awk '{if (length($0) >10) printf "\"%s\",\n",$0}' >> $tmpjs
+
+echo "]" >> $tmpjs
+
+cat $tmpjs2 >> $tmpjs
 
 rm -rf /tmp/_tmpdir
 
--- js/quotes.js
+++ js/quotes.js
@@ -1,19 +1,5 @@
 
  function Start () {
-
-     var mydata = [
-
-    '> 美丽的花开了，可它也会有一天凋谢  ',
-    '> 但是，最后还是只有一死......  ',
-    '> 沙加啊，你忘记了  ',
-    '> 死并不是一切的终结  ',
-    '> 花开了，然后又会凋零  ',
-    '> 春江潮水连海平，海上明月共潮生。  '
-
-
-     ]
- 
-     // pick a random number between 1 and 10
      var myrandom = Math.floor( Math.random() * (mydata.length));
      document.getElementById('quotedisplay').innerHTML = mydata[myrandom]
  }
