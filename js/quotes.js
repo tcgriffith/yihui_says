e55ce09fed420a8f32f25df489a7b928a3a8bbc4
tcgriffith <ztc.ustcer@gmail.com>
2019-12-06 14:55:12 +1000

    Herpderp, shoulda check if it does really compile.

--- js/quotes.js
+++ js/quotes.js
@@ -1,6 +1,4 @@
- import System.IO;
- var fileName = "/yihui_says/data/allposts.min.txt";
-  
+
  function Start () {
  
       // read from filename
@@ -9,7 +7,17 @@
      sr.Close();
   
       // put data from filename into a variable
-     var mydata = fileContents.Split("\n"[0]);
+     var mydata = [
+
+    '> 美丽的花开了，可它也会有一天凋谢  ',
+    '> 但是，最后还是只有一死......  ',
+    '> 沙加啊，你忘记了  ',
+    '> 死并不是一切的终结  ',
+    '> 花开了，然后又会凋零  ',
+    '> 春江潮水连海平，海上明月共潮生。  '
+
+
+     ]
  
      // pick a random number between 1 and 10
      var myrandom = Random.Range(0,mydata.Length);
